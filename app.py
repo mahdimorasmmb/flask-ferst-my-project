b819d3964c8329696ab1b1108bf7f65378bbcb2f
mahdi <mahdimoras@gmail.com>
2020-10-30 22:31:57 +0330

    'AddSECRET_KEY'

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import Development
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+app.config.from_object(Development)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
 
 @app.route('/')
 def index():
-    return "blog home"
+    return "blog home" 
 
 from mod_admin import admin
+from mod_users import users
 
 app.register_blueprint(admin)
+app.register_blueprint(users)
